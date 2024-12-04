f1e47e7949d4c17fec6dcd6eebcead26a9569dc4
iv.kovatchev <iv.kovatchev@gmail.com>
2024-12-04 12:46:03 +0200

    fixing my program.cs to work with 8.0

--- PlatformService/obj/Debug/net8.0/PlatformService.AssemblyInfo.cs
+++ PlatformService/obj/Debug/net8.0/PlatformService.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79dad3b7bb24a87c320ecbdcc4824e162da99c7f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5cce9b5edb2ad7ccb75dbc024b59fc6c4ef9c24")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
